6fc9bfa305d886ccb7edd8f369fb7071234ad21a
Karol Miranda <karolmirandapp@hotmail.com>
2020-07-27 16:25:48 -0300

    exercicio inacabado, fiz até o 2

---
+++ semana3/variaveis/index.js
@@ -0,0 +1,51 @@
+console.log("hello world")
+
+// Exercicio 1  
+// Os valores impressos no console serão: 10 
+//                                        10 5
+// Exercicio 2 
+// O Valor impresso será: 10 10 10
+
+
+
+let nome
+console.log(typeof(nome))
+
+let idade
+console.log(typeof(idade))
+
+// O tipo de variável impresso foi Undefined pois os valores não foram definidos
+
+// Letra D e E:
+nome= "Karoline"
+console.log(typeof(nome))
+idade= 34
+console.log(typeof(idade))
+
+// O tipo das variáveis modificou respectivamente para string, pois o valor foi um texto; e number, pois o valor foi um número.
+
+// Letra F:
+console.log("olá", nome, "você tem", idade, "anos")
+
+
+
+//exercicio numero 2
+const animal= prompt ("Qual o nome do seu 1º animal de estimação?")
+console.log ("Qual o nome do seu 1º animal de estimação?", "resposta" , animal)
+const escola= prompt ("Qual o nome da sua 1ª escola?")
+console.log ("Qual o nome da sua 1ª escola?" , "resposta", escola)
+const comida= prompt ("Qual sua comida favorita?")
+console.log ("Qual sua comida favorita?" , "resposta", comida)
+const cor= prompt ("Qual sua cor favorita?")
+console.log ("Qual sua cor favorita?" , "resposta", cor)
+const bebida= prompt ("Qual sua bebida favorita?")
+console.log ("Qual sua bebida favorita?" ,  "resposta", bebida)
+
+//exercicio numero 3
+let array = ['sushi', 'lamen', 'churrasco', 'massa', 'empada']
+console.log("Essas são as minhas comidas preferidas:")
+console.log(array)[0]
+console.log(array)[1]
+console.log(array)[2]
+console.log(array)[3]
+console.log(array)[4]
